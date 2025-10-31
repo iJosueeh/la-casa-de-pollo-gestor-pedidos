542efd3b6dd0b9fe12c59708c48f97d4cb11db08
IJosueeh01 <royer.tanta27@gmail.com>
2025-10-30 21:36:53 -0500

    feat: config alias @

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': `${process.cwd()}/src`,
+      '@shared': `${process.cwd()}/src/shared`,
+      '@/features': `${process.cwd()}/src/features`,
+      '@/pages': `${process.cwd()}/src/pages`,
+      '@/hooks': `${process.cwd()}/src/hooks`,
+      '@/utils': `${process.cwd()}/src/utils`,
+      '@/types': `${process.cwd()}/src/types`,
+      '@/api': `${process.cwd()}/src/api`
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
