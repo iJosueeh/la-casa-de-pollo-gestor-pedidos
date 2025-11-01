9596215865ca105d56c8351f83f7e898d879eb48
IJosueeh01 <royer.tanta27@gmail.com>
2025-10-31 23:25:44 -0500

    feat: Conflictos corregidos

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
+import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import { AppRouter } from './routes/AppRouter'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
     <AppRouter />
-  </StrictMode>,
+  </React.StrictMode>,
 )
