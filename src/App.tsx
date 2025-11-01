a5c2dd1a523439f6e7339a72d7acd8dd68647b6f
Crismar12 <printza9.08@gmail.com>
2025-10-31 23:07:18 -0500

    conexión a supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
-
-import React from "react";
+import { Outlet } from 'react-router-dom';
 import { Header, Sidebar } from "./shared/components/layout";
+
 
 function App() {
   return (
@@ -10,8 +10,7 @@ function App() {
       <div className="flex flex-1">
         <Sidebar />
         <main className="flex-1 p-6 bg-gray-50">
-          {/* Aquí irá ProductCard y la lista de productos */}
-          <h2 className="text-lg font-bold mb-4">Menú de Productos</h2>
+          <Outlet />
         </main>
       </div>
     </div>
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+
+export const HomePage = () => {
+  return (
+    <div>
+      <h2 className="text-lg font-bold mb-4">Bienvenido a La Casa de Pollo</h2>
+      <p>Selecciona una opción del menú de la izquierda para comenzar.</p>
+    </div>
+  );
+};
